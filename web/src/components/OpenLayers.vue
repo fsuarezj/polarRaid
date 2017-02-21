25aa603e3ca92487bb6b38e35f82cb30eaff2f9b
Fernando Suárez <fsuarezj@gmail.com>
2017-02-21 02:56:47 +0100

    Added stroke with style for GPX and GeoJSON

--- web/src/components/OpenLayers.vue
+++ web/src/components/OpenLayers.vue
@@ -4,6 +4,24 @@
 
 <script>
   import ol from "openlayers"
+  function dashHandDrawn() {
+    let result = [];
+    for (let i = 0; i<30; i++) {
+      if (i % 2)
+        result[i] = Math.round(Math.random()*5 + 12);
+      else {
+        result[i] = Math.round(Math.random()*5 + 5);
+      }
+    }
+    return result;
+  }
+  var lineStyle = new ol.style.Style({
+    stroke: new ol.style.Stroke({
+      color: '#C00',
+      lineDash: dashHandDrawn(),
+      width: 5
+    })
+  });
   export default {
     mounted() {
       this.mapObject = new ol.Map({
@@ -21,10 +39,42 @@
           })
         ],
         view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
-        })
+          center: ol.proj.fromLonLat([26, 68.9]),
+          zoom: 6
+        }),
+        controls: ol.control.defaults().extend([
+          new ol.control.ScaleLine({
+            units: 'degrees'
+          })
+        ])
       });
+      this.mapObject.addLayer(
+        new ol.layer.Vector({
+          source: new ol.source.Vector({
+            url: './src/assets/route.gpx',
+            format: new ol.format.GPX()
+          }),
+          style: lineStyle
+        })
+      );
+      this.mapObject.addLayer(
+        new ol.layer.Vector({
+          source: new ol.source.Vector({
+            url: './src/assets/track.gpx',
+            format: new ol.format.GPX()
+          }),
+          style: lineStyle
+        })
+      );
+      this.mapObject.addLayer(
+        new ol.layer.Vector({
+          source: new ol.source.Vector({
+            url: './src/assets/geo.json',
+            format: new ol.format.GeoJSON()
+          }),
+          style: lineStyle
+        })
+      );
     }
   }
 </script>
