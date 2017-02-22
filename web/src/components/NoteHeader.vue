6253bdac12fa093c197569bc66978b80cbdc9a7d
Fernando Suárez <fsuarezj@gmail.com>
2017-02-22 20:13:28 +0100

    Modified logo

--- web/src/components/NoteHeader.vue
+++ web/src/components/NoteHeader.vue
@@ -1,6 +1,6 @@
 <template lang="html">
-  <div class="note-header">
-    <!-- <img src="src/assets/logo_notebook.png" alt=""> -->
+  <div class="box">
+    <div class="note-header"></div>
   </div>
 </template>
 
@@ -9,21 +9,34 @@ export default {
 }
 </script>
 
-<style scoped>
-  .note-header {
+<style lang="scss" scoped>
+  .box {
     background-image: url("http://localhost:8081/src/assets/logo_notebook.png");
     background-size: contain;
     color: #DDD;
-    width: 350px;
-    height: 350px;
+    width: 20%;
     padding: 10px;
-    margin-top: -15px;
+    margin-top: -2%;
+    margin-left: -1%;
     position: fixed;
     z-index: 10;
     vertical-align: middle;
-    -ms-transform: rotate(-7deg); /* IE 9 */
-    -webkit-transform: rotate(-7deg); /* Chrome, Safari, Opera */
-    transform: rotate(-7deg);
+    -ms-transform: rotate(-12deg); /* IE 9 */
+    -webkit-transform: rotate(-12deg); /* Chrome, Safari, Opera */
+    transform: rotate(-12deg);
+    position: relative;
+  }
+  .box:before {
+    content: "";
+    display: block;
+    padding-top: 97.94%;
+  }
+  .note-header {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
   }
   h1 {
     margin: 0px 100px 0px 0px;
