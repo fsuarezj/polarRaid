34eb7424df00e968f0b6ee1babae1b33b7886c40
Fernando Suárez <fsuarezj@gmail.com>
2017-03-10 18:55:12 +0100

    Filtering feature by layer

--- web/src/components/OlGeojsonLayer.vue
+++ web/src/components/OlGeojsonLayer.vue
@@ -15,7 +15,7 @@
     }),
     'start_sel': new ol.style.Icon({
       src: './src/assets/start.png',
-      color: '#A00',
+      color: '#F00',
       anchor: [0.45, 0.75],
       scale: 0.5
     })
@@ -72,10 +72,14 @@
         let pixel = evt.map.getEventPixel(evt.originalEvent);
         let hit = evt.map.hasFeatureAtPixel(
           pixel,
-          function(layer) {
-            console.log("Testing layer");
-            return true;
-        });
+          {
+            'layerFilter': function(layer) {
+              console.log("Testing layer");
+              return layer === elem.vector;
+            },
+            'hitTolerance': 9
+          }
+        );
         if (hit) {
           evt.map.getTarget().style.cursor = 'pointer';
           evt.map.forEachFeatureAtPixel(pixel,
@@ -83,12 +87,6 @@
               feature.getStyle().setImage(images['start_sel']);
               feature.changed();
               elem.changedFeatures.push(feature);
-            },
-            {
-              layerFilter: layer => {
-                return layer === elem.vector;
-              },
-              hitTolerance: 5
             }
           );
         } else {
