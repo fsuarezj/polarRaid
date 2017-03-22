4cb249aa55d288d2de4c0e83b26e57f99f7eed21
Fernando Suárez <fsuarezj@gmail.com>
2017-03-22 01:04:21 +0100

    Added browser and android platforms

--- track_app/src/main.js
+++ track_app/src/main.js
@@ -10,10 +10,9 @@ import Framework7Vue from 'framework7-vue'
 // Import F7 iOS Theme Styles
 import Framework7Theme from 'framework7/dist/css/framework7.ios.min.css'
 import Framework7ThemeColors from 'framework7/dist/css/framework7.ios.colors.min.css'
-/* OR for Material Theme:
+/* OR for Material Theme: */
  import Framework7Theme from 'framework7/dist/css/framework7.material.min.css'
  import Framework7ThemeColors from 'framework7/dist/css/framework7.material.colors.min.css'
- */
 
 // Import App Custom Styles
 import AppStyles from './assets/static/sass/main.scss'
@@ -22,7 +21,7 @@ import AppStyles from './assets/static/sass/main.scss'
 import Routes from './routes.js'
 
 // Import App Component
-import App from './main.vue'
+import App from './app.vue'
 
 // Init F7 Vue Plugin
 Vue.use(Framework7Vue)
@@ -35,7 +34,7 @@ new Vue({
 	framework7: {
 		root: '#app',
 		/* Uncomment to enable Material theme: */
-		// material: true,
+		material: true,
 		routes: Routes
 	},
 	// Register App Component
