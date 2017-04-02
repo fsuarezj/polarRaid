bbfcbe1c46214a5b331e3732d10a1ea2593e583a
Fernando Suárez <fsuarezj@gmail.com>
2017-04-02 18:49:30 +0200

    Added polarRaidBeta firebase database

--- src/components/mixins/FirebaseDB.js
+++ src/components/mixins/FirebaseDB.js
@@ -1,6 +1,6 @@
 import Firebase from 'firebase'
 
-let config = {
+let configTest = {
   apiKey: "AIzaSyBrnMRULFyfa0H7mDcQ3Yb22QyJtBEHSd0",
   authDomain: "my-vue2-firebase.firebaseapp.com",
   databaseURL: "https://my-vue2-firebase.firebaseio.com",
@@ -8,9 +8,18 @@ let config = {
   messagingSenderId: "282000909832"
 };
 
-let app = Firebase.initializeApp(config);
-let firebaseDB = app.database();
+let configPolarRaidBeta = {
+  apiKey: "AIzaSyAFs_c-JhZrYY8RoJtqKcxSIfaZQvJ8VVw",
+  authDomain: "polarraid-83d2f.firebaseapp.com",
+  databaseURL: "https://polarraid-83d2f.firebaseio.com",
+  projectId: "polarraid-83d2f",
+  storageBucket: "polarraid-83d2f.appspot.com",
+  messagingSenderId: "609239958081"
+};
 
-export const getFirebaseRef = function(ref) {
+
+export const getFirebaseRef = function(ref, config = configTest) {
+  let app = Firebase.initializeApp(config);
+  let firebaseDB = app.database();
   return firebaseDB.ref(ref);
 }
